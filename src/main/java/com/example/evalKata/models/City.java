fb92ecfe670bc6d70c15b55044f939970cb25b5d
JEKOLLE <111181197+JEKOLLE@users.noreply.github.com>
2023-02-22 17:04:24 +0100

    test fail

---
+++ src/main/java/com/example/evalKata/models/City.java
@@ -0,0 +1,15 @@
+package com.example.evalKata.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class City {
+	
+	List<String> cities = new ArrayList<>();
+	
+	public String getCity() {
+		return null;
+		
+	}
+
+}
