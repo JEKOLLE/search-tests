d8a87821a00e04b2dddd2c65339bf784ca496e7d
JEKOLLE <111181197+JEKOLLE@users.noreply.github.com>
2023-02-22 16:39:53 +0100

    first commit

---
+++ src/main/java/com/example/evalKata/EvalKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.evalKata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evalKata/EvalKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evalKata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
