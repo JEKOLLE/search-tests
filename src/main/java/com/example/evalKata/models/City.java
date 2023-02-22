59d4f3bdbc6204218351778b65acbc4e9d195e67
JEKOLLE <111181197+JEKOLLE@users.noreply.github.com>
2023-02-22 18:53:30 +0100

    assert find by contains

--- src/main/java/com/example/evalKata/models/City.java
+++ src/main/java/com/example/evalKata/models/City.java
@@ -1,11 +1,13 @@
 package com.example.evalKata.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class City {
 	
-	List<String> cities = new ArrayList<>();
+	List<String> cities = Arrays.asList("Paris","Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", 
+			                            "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
 	
 	public List<String> getCityByString(String city) {
 		List<String> list = new ArrayList<>();
