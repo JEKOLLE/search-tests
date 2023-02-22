ce1f725de5e60d398168bb3b75557f0e102e6c9f
JEKOLLE <111181197+JEKOLLE@users.noreply.github.com>
2023-02-22 18:02:39 +0100

    created function

--- src/main/java/com/example/evalKata/models/City.java
+++ src/main/java/com/example/evalKata/models/City.java
@@ -1,13 +1,31 @@
 package com.example.evalKata.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class City {
 	
-	List<String> cities = new ArrayList<>();
+	List<String> cities = Arrays.asList("Paris","Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", 
+			                            "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
 	
-	public String getCity() {
+	public List<String> getCityByString(String city) {
+		List<String> list = new ArrayList<>();
+		
+		if(city.length()>=2) {
+			for(String ville: cities){
+				if(ville.toLowerCase().startsWith(city.toLowerCase()) || ville.toLowerCase().contains(city.toLowerCase()))
+					list.add(ville);
+			}
+			return list;
+		}
+		
+		if(city.equalsIgnoreCase("*")){
+			for(String ville: cities)
+				list.add(ville);
+			return list;
+		}
+
 		return null;
 		
 	}
